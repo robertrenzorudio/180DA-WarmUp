aeee20bc2831a3ef4ee39144e07f7a399838869c
Robert Renzo Rudio <robertrenzorudio@gmail.com>
2020-10-06 22:27:10 -0700

    Added object_tracking.py and dominant_color.py

---
+++ dominant_color.py
@@ -0,0 +1,86 @@
+"""
+NAME: Robert Renzo Rudio
+ID: 005366334
+
+References: 
+- getting region of interest in opencv2:
+https://techtutorialsx.com/2019/11/24/python-opencv-getting-region-of-interest/
+"""
+
+import argparse
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.cluster import KMeans
+import sys
+
+def dominant_color(cap: cv2.VideoCapture, pt1: tuple, pt2: tuple):
+    x1, y1 = pt1
+    x2, y2 = pt2
+
+    fourcc = cv2.VideoWriter_fourcc(*'avc1')
+    size = (int(cap.get(3)), int(cap.get(4)))
+    out = cv2.VideoWriter('output.mp4', fourcc, 20.0, size)
+
+    while(True):
+        _, frame = cap.read()
+       
+        # Get region of interest from frame. 
+        roi = frame[y1:y2, x1:x2]
+        roi = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
+
+        # Convert roi to 2d array.
+        x, y, z = roi.shape
+        roi_2d = roi.reshape(x*y, z)
+       
+        # Perfrom K-means clustering to find dominant color.
+        kmeans = KMeans(n_clusters=3)
+        kmeans.fit(roi_2d)
+        most_label = np.bincount(kmeans.labels_).argmax()
+        dc = [int(i) for i in kmeans.cluster_centers_[most_label]]
+        
+        # Draw rectangle and print dominant color.
+        rect_color = (dc[2], dc[1], dc[0])
+        rect = cv2.rectangle(frame, (x1, y1), (x2, y2), rect_color, 2)
+        cv2.putText(rect, "Dominant RGB: {}".format(dc), (x1, y1), 0, 0.5, rect_color)
+        print(dc)
+
+        out.write(frame)
+        cv2.imshow("frame", frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    out.release()
+    return 
+
+def main():
+    usg_msg = "python3 {} -p [Starting-Point: x,y] -w [Width] -l [Height]\n".format(sys.argv[0])
+    descr = "Print Dominant Color Inside a Rectangle."
+    parser = argparse.ArgumentParser(usage=usg_msg, description=descr)
+    parser.add_argument("-p", type=str, required=False, help="Starting point of Rectangle (Centered by Default)")
+    parser.add_argument("-w", type=str, required=False, help="Width of Rectangle (Default = 250)")
+    parser.add_argument("-l", type=str, required=False, help="Height of Rectangle (Default = 250)")
+    args = parser.parse_args()
+    
+
+    # Parse Arguments.
+    w = int(args.w) if args.w else 250
+    h = int(args.l) if args.l else 250 
+    cap = cv2.VideoCapture(0)
+
+    if args.p:
+        pt1 = args.p.split(",")
+        pt1 = (int(pt1[0]), int(pt1[1]))
+    else:
+        pt1 = (int(cap.get(3))//2 - w//2, int(cap.get(4))//2 - h//2)
+
+    pt2 = (pt1[0] + w, pt1[1] + h)
+    
+    dominant_color(cap, pt1, pt2)
+   
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
---
+++ object_tracking.py
@@ -0,0 +1,83 @@
+"""
+NAME: Robert Renzo Rudio
+ID: 005366334
+
+References:
+ - Changing color-space:
+ https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/
+ py_imgproc/py_colorspaces/py_colorspaces.html#converting-colorspaces
+
+ - Finding contours and bounding rectangle:
+ https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/
+ py_imgproc/py_contours/py_contour_features/py_contour_features.html#contour-features
+"""
+
+import argparse
+import cv2
+import numpy as np
+import sys
+
+def track(c_space: str, lower: np.uint8, upper: np.int8):
+    cap = cv2.VideoCapture(0)
+
+    fourcc = cv2.VideoWriter_fourcc(*'avc1')
+    size = (int(cap.get(3)), int(cap.get(4)))
+    out = cv2.VideoWriter('output.mp4', fourcc, 20.0, size)
+
+    while (True):
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        if c_space == "RGB":
+            rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            mask = cv2.inRange(rgb, lower, upper)
+        
+        else:
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            mask = cv2.inRange(hsv, lower, upper)
+
+        res = cv2.bitwise_and(frame, frame, mask=mask) 
+        countours, _ = cv2.findContours(mask, 1, 2)
+
+        for cnt in countours:
+            area = cv2.contourArea(cnt)
+            if area > 1000:
+                x, y, w, h = cv2.boundingRect(cnt)
+                cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+
+        out.write(res)
+        cv2.imshow("frame", frame)
+        cv2.imshow("res", res)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+def main():
+    usg_msg = "python3 {} -c [RGB|HSV] -l [Lower-Range], -u [Upper-Range]".format(sys.argv[0])
+    descr = "Track Object Using RGB or HSV Values."
+    parser = argparse.ArgumentParser(usage=usg_msg, description=descr)
+    parser.add_argument("-c", type=str, required=True, help="Color Space to be Used (RGB or HSV)")
+    parser.add_argument("-l", type=str, required=True, help="Lower Range of the Color in CSV Format")
+    parser.add_argument("-u", type=str, required=True, help="Upper Range of the Color in CSV Format")
+    args = parser.parse_args()
+
+    # Parse arguments.
+    c_space = str.upper(args.c)
+    if c_space == "RGB" and c_space == "HSV":
+        sys.exit(1)
+
+    lower = args.l.split(',')
+    lower = np.array([int(i) for i in lower], np.uint8)
+    upper = args.u.split(',')
+    upper = np.array([int(i) for i in upper], np.uint8)
+
+    track(c_space, lower, upper)
+    
+if __name__ == "__main__":
+    main()
